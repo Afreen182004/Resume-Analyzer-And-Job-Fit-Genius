35c2e6a41690d49c7e9babc971ab5d11f2ba3f10
Afreen A <afreenshanti88@gmail.com>
2026-02-04 11:44:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from logic import *
 st.set_page_config(page_title="Resume Analyzer", layout="wide")
 
 st.title("🚀 Resume Analyzer & Job Fit Genius")
-st.write("Upload your resume and paste the Job Description to get a complete AI-based analysis")
+st.write("Upload your resume and paste the Job Description to get a complete analysis")
 
 left_col, right_col = st.columns(2)
 
@@ -113,6 +113,7 @@ if st.button("🔍 Analyse Resume"):
             st.warning("🟡 Candidate kept in WAITING LIST")
         else:
             st.error("❌ Candidate REJECTED for this Job")
+
 
 
 
